18016f2764e827ae3f3db970ec355d338b1a6998
MartinFerret <ferretmartin91@gmail.com>
2022-03-07 09:57:28 +0100

    done

--- src/Controller/Admin/OperationCrudController.php
+++ src/Controller/Admin/OperationCrudController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Operation;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
@@ -22,12 +23,20 @@ class OperationCrudController extends AbstractCrudController
     {
         return [
             IdField::new('id')->hideOnForm(),
-            BooleanField::new('type'),
-            MoneyField::new('amount')->setCurrency('EUR'),
-            DateField::new('createdAt'),
-            AssociationField::new('pot')->autocomplete(),
-            AssociationField::new('user')->autocomplete(),
+            BooleanField::new('type', 'Type'),
+            MoneyField::new('amount', 'Montant')->setCurrency('EUR'),
+            DateField::new('createdAt', 'Créée le'),
+            AssociationField::new('pot', 'Cagnotte')->autocomplete(),
+            AssociationField::new('user', 'Utilisateur')->autocomplete(),
         ];
+    }
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInSingular('Opération')
+            ->setEntityLabelInPlural('Opérations')
+        ;
     }
     
 }
--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Admin;
 
 use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
@@ -10,7 +11,6 @@ use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class UserCrudController extends AbstractCrudController
@@ -26,18 +26,26 @@ class UserCrudController extends AbstractCrudController
         return [
             IdField::new('id')->hideOnForm(),
             EmailField::new('email'),
-            ArrayField::new('roles'),
-            TextField::new('firstname'),
-            TextField::new('lastname'),
-            DateField::new('birthDate'),
-            BooleanField::new('status'),
-            TextField::new('phone'),
-            DateField::new('createdAt'),
-            DateField::new('updatedAt')->hideOnForm(),
+            ArrayField::new('roles', 'Rôle'),
+            TextField::new('firstname', 'Prénom'),
+            TextField::new('lastname', 'Nom'),
+            DateField::new('birthDate', 'Date de naissance'),
+            BooleanField::new('status', 'Statut'),
+            TextField::new('phone', 'Numéro de téléphone'),
+            DateField::new('createdAt', 'Créé le'),
+            DateField::new('updatedAt', 'Modifié le')->hideOnForm(),
             TextField::new('iban'),
             TextField::new('bic'),
-            AssociationField::new('pots')->autocomplete(),
+            AssociationField::new('pots', 'Cagnottes')->autocomplete(),
         ];
+    }
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInSingular('Utilisateur')
+            ->setEntityLabelInPlural('Utilisateurs')
+        ;
     }
     
 }
