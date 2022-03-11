69d747dc1b29afc50dfaa05a83959bad7d9f5259
TristanBonnal <bonnal.tristan@hotmail.fr>
2022-03-11 13:09:21 +0100

    mailjet in progress

--- src/Controller/MailerController.php
+++ src/Controller/MailerController.php
@@ -3,34 +3,45 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Mailer\MailerInterface;
-use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
+use Mailjet\Resources;
+use Mailjet\Client;
+
 
 class MailerController extends AbstractController
 {
+
     /**
      * Undocumented function
      *
-     * @param MailerInterface $mailer
-     * @return Response
-     * @Route("/email", name="email")
+     * @Route("/email",name="email")
+     * @return void
      */
-    public function sendEmail(MailerInterface $mailer)
-    {
-        $email = (new Email())
-            ->from('jea@gmail.com')
-            ->to('bonnal.tristan@hotmail.fr')
-            //->cc('cc@example.com')
-            //->bcc('bcc@example.com')
-            //->replyTo('fabien@example.com')
-            //->priority(Email::PRIORITY_HIGH)
-            ->subject('Réinitialisaiton')
-            ->text('Sending emails is fun again!')
-            ->html('<p>See Twig integration for better HTML integration!</p>');
-
-        $mailer->send($email);
-        // return $this->redirectToRoute('admin');
+    public function sendmail() {
+        $mj = new Client('54abf7bd7c959b059abdee6778722a2c','89a12b1d1e22e0c08167316926b02e1e',true,['version' => 'v3.1']);
+        $body = [
+          'Messages' => [
+            [
+              'From' => [
+                'Email' => "bonnal.tristan@hotmail.fr",
+                'Name' => "Tristan"
+              ],
+              'To' => [
+                [
+                  'Email' => "bonnal.tristan@hotmail.fr",
+                  'Name' => "Tristan"
+                ]
+              ],
+              'Subject' => "Greetings from Mailjet.",
+              'TextPart' => "My first Mailjet email",
+              'HTMLPart' => "<h3>Dear passenger 1, welcome to <a href='https://www.mailjet.com/'>Mailjet</a>!</h3><br />May the delivery force be with you!",
+              'CustomID' => "AppGettingStartedTest"
+            ]
+          ]
+        ];
+        $response = $mj->post(Resources::$Email, ['body' => $body]);
+        $response->success() && var_dump($response->getData());
 
     }
+
 }
